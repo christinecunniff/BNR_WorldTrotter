070adda4106faa680104f1430312ecf2d023ab7c
Christine Cunniff <christinecunniff@gmail.com>
2016-11-08 16:00:42 -0500

    Update celsius label with calculation from fahrenheit input.

--- BNR_WorldTrotter/BNR_WorldTrotter/ConversionViewController.swift
+++ BNR_WorldTrotter/BNR_WorldTrotter/ConversionViewController.swift
@@ -5,14 +5,34 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    @IBAction func fahrenheitFieldEditingChange(textField: UITextField) {
-        
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = textField.text
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
         } else {
             celsiusLabel.text = "???"
         }
-        
+    }
+    
+    @IBAction func fahrenheitFieldEditingChange(textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
     }
     
     @IBAction func dismissKeyboard(sender: AnyObject) {
